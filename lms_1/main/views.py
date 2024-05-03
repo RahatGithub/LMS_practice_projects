43112c503cfbc287fa8c541ec1784b5b1c4d3c28
Rahat Ahmed Chowdhury <rahatahmedchowdhuryrac@gmail.com>
2024-05-04 01:18:15 +0600

    Teachers list api done

--- lms_1/main/views.py
+++ lms_1/main/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from . import models 
+from .serializers import TeacherSerializer 
 
-# Create your views here.
+
+class TeacherList(APIView):
+    def get(self, request):
+        teachers = models.Teacher.objects.all() 
+        serializer = TeacherSerializer(teachers, many=True)
+        return Response(serializer.data)
